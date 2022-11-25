d7336231549d911bcc7f3b06503a1269812d156c
kconcha <kconcha@bancoguayaquil.com>
2022-11-24 23:40:37 -0500

    ahorcado

---
+++ ahorcado.py
@@ -0,0 +1,51 @@
+import random as rd
+#Modulo random Piogram
+palabras=["Axell","Piogram",
+    "David","Gabriela", "Andres",
+    "Capuchino","Fernando"]
+secreto = rd.choice(palabras).lower()
+print("secreto:",secreto)
+vocales="aeiou"
+letra = rd.choice(secreto)
+while letra not in vocales:
+    letra = rd.choice(secreto)
+print("Letra:", letra)
+
+pista=[]
+for caracter in secreto:
+    if caracter != letra:
+        pista.append("-")
+    else:
+        pista.append(letra)
+
+turnos= 2 * len(secreto) 
+while turnos!=0 and "".join(pista) != secreto:
+    print("".join(pista))
+    adivina = input("Ingrese una letra: ").lower()#E
+    if adivina in secreto:
+        for i in range(len(secreto)):#ESPOLE
+            letraSecreto = secreto[i] #E
+            if adivina == letraSecreto:
+                #E--O-E
+                pista[i] = adivina
+    turnos-=1        
+            
+
+
+
+
+        
+
+#ESPOL
+
+#P
+#--PO-
+
+
+
+
+        
+
+
+
+
---
+++ prueba.py
@@ -0,0 +1,3 @@
+numero=1
+print(numero+2) #3
+print(numero) #1
